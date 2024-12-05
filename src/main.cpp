a07fe59bc228453142dedcee6843ed1dc621bf0d
Nicolas PUTON <n.puton@orange.fr>
2024-12-05 16:10:20 +0100

    fix: Sécurisation des entrées utilisateur

--- src/main.cpp
+++ src/main.cpp
@@ -7,63 +7,56 @@ using namespace std;
 int main() {
     Catalogue c = Catalogue();
 
-    int choice = -1;
-
-    while (1) {
-        #ifdef _WIN32
-        cout << choice;
-            system("cls");
-        #else
-            system("clear");
-        #endif
-        
-        if (choice == -1)
+    char choice;
+    int first = 1;
+    
+    do 
+    {
+        ClearScreen();
+        if (first)
         {
-            cout << "~~~~~~~~~~~ Bienvenue dans notre agence de voyages! ~~~~~~~~~~~\n";
+            cout << "~~~~~~~~ Bienvenue dans notre agence de voyages! ~~~~~~~~\n";
+            first = 0;
         }
 
 
         cout << "\n\t~~~~~~~~~~~ Menu ~~~~~~~~~~~\nEntrez votre choix :\n\t0. Afficher le catalogue\n\t1. Ajouter un trajet au catalogue\n\t2. Supprimer un trajet du catalogue\n\t3. Quitter\n";
+        cout << "Votre choix : ";
         cin >> choice;
+        // Si l'entrée est invalide
+        
+        ClearScreen();
 
-        while (cin.fail() || (choice != 0 && choice != 1 && choice != 2 && choice != 3))
-        {
-            #ifdef _WIN32
-                system("cls");
-            #else
-                system("clear");
-            #endif
-            cout <<     "--------------------/!\\------------------\n";
-            cout << "Vous devez choisir une valeur entre 0 et 3!\nEntrez votre choix :\n\t0. Afficher le catalogue\n\t1. Ajouter un trajet au catalogue\n\t2. Supprimer un trajet du catalogue\n\t3. Quitter\n";
-
-            cin >> choice; 
+        if (!((int)choice >= 48 && (int)choice <= 51)) {
+            cout << "\n--------------------/!\\------------------\n";
+            cout << "Erreur : vous devez choisir une valeur entre 0 et 3 !\n";
+            cin.clear();
+            cin.ignore(); 
+            cout << "Appuyez sur Entrée pour recommencer...";
+            cin.get(); 
+            continue;
         }
-
-        #ifdef _WIN32
-            system("cls");
-        #else
-            system("clear");
-        #endif
 
         switch (choice) {
-            case 0: // Afficher
+            case '0': // Afficher
                 c.AfficherCatalogue();
                 break;
-            case 1: // Créer
+            case '1': // Créer
                 c.CreerTrajet();
                 break;
-            case 2: // Supprimer
+            case '2': // Supprimer
                 cout << "Fonctionnalité à implémenter." << endl;
                 break;
-            case 3: // Quitter
+            case '3': // Quitter
                 cout << "\n\t~~~~~~~~~~~ A bientôt! ~~~~~~~~~~~\n";
                 exit(EXIT_SUCCESS);
+                break;
         }
 
-        // Pause avant de revenir au menu
         cout << "\n\nAppuyez sur Entrée pour revenir au menu...";
         cin.ignore();
         cin.get();
-    }
+    } while(1);
+    
     return 0;
 }
